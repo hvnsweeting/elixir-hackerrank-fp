a905e75fe58c3b32ace5a030dde49afdf1156b7a
Hung Nguyen Viet <hvn@familug.org>
2017-05-19 12:27:35 +0700

    Oh, we need also handle neg number

--- sum_of_odd.exs
+++ sum_of_odd.exs
@@ -2,7 +2,7 @@ defmodule Solution do
   def main() do
     data = IO.read(:stdio, :all) |> String.trim |> String.split([" ", "\n"])
           |> Enum.map(fn(x) -> String.to_integer(x) end)
-          |> Enum.filter(fn(x) -> rem(x, 2) == 1 end)
+          |> Enum.filter(fn(x) -> abs(rem(x, 2)) == 1 end)
           |> Enum.sum
 	IO.puts data
   end
