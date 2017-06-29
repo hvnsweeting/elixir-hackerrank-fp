93e2276915f27809bb7ef30d6a72d0c437834043
Viet Hung Nguyen <hvn@familug.org>
2017-06-29 11:56:24 +0700

    Refactor

--- prefix_compression.exs
+++ prefix_compression.exs
@@ -1,38 +1,41 @@
 # https://www.hackerrank.com/challenges/string-reductions?h_r=next-challenge&h_v=zen
 defmodule Solution do
 
-  def solve([[h1|t1], [h2|t2]]) do
+  def find_suffix([[h1|t1], [h2|t2]]) do
     if h1 == h2 do
-      solve([t1, t2])
+      find_suffix([t1, t2])
     else
       IO.puts Integer.to_string(length(t1) + 1) <> " " <> List.to_string([h1|t1])
       IO.puts Integer.to_string(length(t2) + 1) <> " " <> List.to_string([h2|t2])
     end
   end
 
-  def solve([[], s2]) do
+  def find_suffix([[], s2]) do
     IO.puts 0
     IO.puts(Integer.to_string(length(s2)) <> " " <> List.to_string(s2))
   end
 
-  def solve([s1, []]) do
+  def find_suffix([s1, []]) do
     IO.puts(Integer.to_string(length(s1)) <> " " <> List.to_string(s1))
     IO.puts 0
   end
 
 
 
-  def msolve([l1, l2]) do
-    prefix = Enum.zip(l1, l2) |> Enum.take_while(fn ({x1, x2}) -> x1 == x2 end)
-    p = prefix |> Enum.map(fn ({x1, _}) -> x1 end) |> List.to_string
-    IO.puts(Integer.to_string(String.length(p)) <> " " <> p)
+  def find_prefix([l1, l2]) do
+    prefix = Enum.zip(l1, l2)
+               |> Enum.take_while(fn ({x1, x2}) -> x1 == x2 end)
+               |> Enum.map(fn ({x1, _}) -> x1 end) |> List.to_string
+    IO.puts(Integer.to_string(String.length(prefix)) <> " " <> prefix)
 
-    solve([l1, l2])
+    find_suffix([l1, l2])
   end
 
   def main() do
-    lines = String.strip(IO.read(:stdio, :all))
-    lines |> String.split("\n") |> Enum.map(&(String.to_charlist(&1))) |> msolve
+    String.strip(IO.read(:stdio, :all))
+      |> String.split("\n")
+      |> Enum.map(&String.to_charlist/1)
+      |> find_prefix
   end
 end
 Solution.main()
